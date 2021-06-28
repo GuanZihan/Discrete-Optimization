7708f7b165e4db8f67ea2bd99612bd34be591a49
jmgzh@outlook.com <jmgzh123@outlook.com>
2021-06-28 22:58:31 +0100

    tsp

--- Assignment/tsp/solver.py
+++ Assignment/tsp/solver.py
@@ -9,13 +9,12 @@ Point = namedtuple("Point", ['x', 'y'])
 
 
 def length(point1, point2):
-    return math.sqrt((point1.x - point2.x)**2 + (point1.y - point2.y)**2)
+    return math.sqrt((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2)
 
 
 def solve_MIP(points, node_count):
     V = [points[0]]
     solution = [0]
-
 
     D = [[0 for i in range(node_count)] for j in range(node_count)]
     S = points[1:]
@@ -39,7 +38,7 @@ def solve_MIP(points, node_count):
 
 
 def findMinDistance(point, S, visited):
-    min_d = 999
+    min_d = 99999
     ret_index = 0
     for index, target in enumerate(S):
         if length(point, target) < min_d and index not in visited:
@@ -47,6 +46,7 @@ def findMinDistance(point, S, visited):
             ret_index = index
     visited.append(ret_index)
     return min_d, ret_index
+
 
 def solve_it(input_data):
     # Modify this code to run your optimization algorithm
@@ -57,7 +57,7 @@ def solve_it(input_data):
     nodeCount = int(lines[0])
 
     points = []
-    for i in range(1, nodeCount+1):
+    for i in range(1, nodeCount + 1):
         line = lines[i]
         parts = line.split()
         points.append(Point(float(parts[0]), float(parts[1])))
@@ -77,11 +77,12 @@ import sys
 
 if __name__ == '__main__':
     import sys
+
     if len(sys.argv) > 1:
         file_location = sys.argv[1].strip()
         with open(file_location, 'r') as input_data_file:
             input_data = input_data_file.read()
         print(solve_it(input_data))
     else:
-        print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/tsp_51_1)')
-
+        print(
+            'This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/tsp_51_1)')
